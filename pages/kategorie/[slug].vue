66b69ea239bcb7daad3e2270a4eaf5f478964b90
David Ovčačík <david.ovcacik@gmail.com>
2025-06-01 20:09:23 +0200

    Add prev/next arrows to photo modal

--- pages/kategorie/[slug].vue
+++ pages/kategorie/[slug].vue
@@ -7,7 +7,7 @@
     </AppHeader>
     <main class="container mx-auto my-10">
       <div class="grid grid-cols-6 mt-10">
-        <div class="relative group cursor-pointer" v-for="image in gallery" :key="image" @click="showDialog(image)">
+        <div class="relative group cursor-pointer" v-for="(image, index) in gallery" :key="image" @click="showDialog(index)">
           <NuxtImg :src="image" sizes="300px" class="object-contain w-full" />
           <div class="absolute inset-0 bg-[color:--theme-color] opacity-0 group-hover:opacity-50" />
         </div>
@@ -26,32 +26,41 @@
       <div class="container mx-auto flex justify-center h-dvh py-16">
         <div class="flex flex-col items-center gap-8 w-full">
           <div class="relative grow w-full">
-            <NuxtImg :src="currentImage" sizes="600px" class="absolute inset-0 size-full object-contain" />
+            <NuxtImg :src="cycleList.state.value" sizes="600px" class="absolute inset-0 size-full object-contain" />
             <NuxtImg :src="watermarkSrc" class="absolute inset-0 size-full object-contain" />
           </div>
-          <button class="border border-black rounded-full bg-white px-1" @click="download(currentImage)">Stáhnout</button>
+          <button class="border border-black rounded-full bg-white px-1" @click="download(cycleList.state.value)">Stáhnout</button>
         </div>
       </div>
+      <button class="absolute top-1/2 left-8 -translate-y-1/2 text-white" @click="cycleList.prev()">
+        <Icon name="ri:arrow-left-s-line" class="text-4xl" />
+      </button>
+      <button class="absolute top-1/2 right-8 -translate-y-1/2 text-white" @click="cycleList.next()">
+        <Icon name="ri:arrow-right-s-line" class="text-4xl" />
+      </button>
     </div>
   </div>
 </template>
 
 <script setup lang="ts">
-  import slugs from '~/config/categories.json';
+  import categories from '~/config/categories.json';
+  import {useCycleList} from '@vueuse/core';
+  import {useRouteParams} from '@vueuse/router';
 
-  const route = useRoute();
+  const slug = useRouteParams<keyof typeof categories>('slug')
 
-  const categoryData = slugs[route.params.slug];
+  const isDialogVisible = ref(false);
 
-  const {data: gallery} = useFetch(`/api/images/category/${route.params.slug}/gallery`, {default: () => []});
+  const categoryData = categories[slug.value];
+
+  const {data: gallery} = useFetch(`/api/images/category/${slug.value}/gallery`, {default: () => []});
 
   const watermarkSrc = computed(() => `/img/watermark/${categoryData.watermark}.png`);
 
-  const currentImage = ref<any>(null);
-  const isDialogVisible = ref(false);
+  const cycleList = useCycleList(gallery);
 
-  function showDialog(image: any) {
-    currentImage.value = image;
+  function showDialog(index: number) {
+    cycleList.go(index);
     isDialogVisible.value = true;
   }
 
